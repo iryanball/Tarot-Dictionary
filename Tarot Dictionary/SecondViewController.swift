414c014b76f71cfdace966168845d6fe8b5782e0
Ryan Ball <ryanball@ryans-imac.local>
2017-07-02 11:59:35 +0100

    Initial Commit

---
+++ Tarot Dictionary/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Tarot Dictionary
+//
+//  Created by Ryan Ball on 02/07/2017.
+//  Copyright © 2017 Ryan Ball. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
