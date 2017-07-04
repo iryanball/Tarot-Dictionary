5382c29607693a603765c3f2dba034508f658c9a
Ryan Ball <ryanball@Ryans-iMac.local>
2017-07-04 18:14:51 +0100

    Sticker pack added

---
+++ Tarot Dictionary/Tarot.swift
@@ -0,0 +1,17 @@
+//
+//  Tarot.swift
+//  Tarot Dictionary
+//
+//  Created by Ryan Ball on 02/07/2017.
+//  Copyright © 2017 Ryan Ball. All rights reserved.
+//
+
+import UIKit
+
+struct Tarot {
+    
+    var tarotName: String
+    var tarotImage: UIImage
+    var tarotDescription: String
+    
+}
